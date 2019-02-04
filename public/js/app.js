864f5865e8b8fbcb35fef63db56a2b5833e0ca25
jeroentvb <jeroentvb@hotmail.com>
2019-02-04 12:11:34 +0100

    Testing cors on github pages

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
+/* global fetch */
+
+'use strict'
+
 console.log('init!')
+
+const url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY'
+
+fetch(url)
+  .then(res => console.log(res.json()))
+  .catch(err => console.error(err))
