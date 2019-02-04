b127916efc49711fbc261a4e127d28e05b204f11
Steven Whitton <stevenawhitton@gmail.com>
2019-02-04 16:30:07 +0100

    Added sprites and input functionality

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -1,7 +1,18 @@
 console.log('Hello World')
 
 var main = document.querySelector('main')
-var url = 'https://api.pokemontcg.io/v1/cards/base2-60'
+var currentNumber = 60
+var url = 'https://api.pokemontcg.io/v1/cards/base2-' + currentNumber
+var input = document.querySelector('input')
+
+input.addEventListener('submit', () => {
+  currentNumber = input.value
+  url = url
+  console.log(currentNumber)
+  console.log(url)
+  request.open('GET', url, true)
+  request.send();
+ })
 
 
 var request = new XMLHttpRequest()
@@ -11,7 +22,7 @@ request.onload = function() {
       var data = JSON.parse(request.responseText)
       var currentCard = data.card.name + ' from ' + data.card.series + ' ' + data.card.set
       document.title = currentCard
-      document.querySelector('h1').innerText = 'Current Card is '+ currentCard
+      document.querySelector('h1').innerText = data.card.name + ' #' + data.card.number + ' ' + data.card.set
       handleData(data)
   } else {
     console.log("Error!")
@@ -21,16 +32,32 @@ request.onload = function() {
 function handleData(data){
   console.log('data is handeled')
 
-  var attacks = ''
-  data.card.attacks.forEach( function(attack) {
-    attacks +=
-      `<p>${attack.cost}</p>
-      <h3>${attack.name}</h3>
-      <p>${attack.text}</p>`
+  function renderAttacks() {
+    var attacks = ''
+    data.card.attacks.forEach( function(attack) {
+      attacks +=
+        `<section class='singleAttack'>
+          ${costToImage(attack.cost)}
+          <h3>${attack.name}</h3>
+          <p>${attack.text}</p>
+          <h3 class='damage'>${attack.damage}</h3>
+        </section>
+          `
+      })
+
+      return attacks
+    }
+
+  function costToImage(cost){
+    var totalCost = ''
+    cost.forEach(function(element) {
+      totalCost +=
+      `<i class='energy ${element.toLowerCase()}'></i>`
     })
+    return totalCost
+  }
 
   var format =`
-  <h2>${data.card.name} ${data.card.number} ${data.card.series} ${data.card.set}</h2>
   <section class='left half'>
     <img class='previewImage' src='${data.card.imageUrl}'/>
   </section>
@@ -42,7 +69,7 @@ function handleData(data){
       <p>Type: ${data.card.types}</p>
     </section>
     <section class='attack'>
-    ${attacks}
+    ${renderAttacks()}
   </section>
   `
 
