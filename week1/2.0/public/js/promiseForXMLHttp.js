055533cb8e0be5235f044049d6abe19d3118a1b1
Stan Ooms <35112551+Stanargy@users.noreply.github.com>
2019-02-08 14:48:45 +0100

    added recipeShowcase. makes api call upon landing on site.

--- week1/2.0/public/js/promiseForXMLHttp.js
+++ week1/2.0/public/js/promiseForXMLHttp.js
@@ -1,5 +1,5 @@
 var endpoint = 'https://www.food2fork.com/api/search';
-var key = '828761ceff9d459c0761d86f78412455';
+var key = 'd0f93ac147b9f574fcbb3605f9619659';
 var url = endpoint + '?key=' + key;
 
 var searchSubject = document.querySelector('#searchInput');
@@ -10,11 +10,11 @@ submitButton.addEventListener('click', submitted);
 
 function submitted() {
     console.log('click works!' + "U searched for meals with: " + searchSubject.value);
-    fireSearchForRecipe(searchSubject.value).then((res)=>{
-       // console.log(res);
+    fireSearchForRecipe(searchSubject.value).then((res) => {
+        // console.log(res);
         var JSONText = JSON.parse(res)
         return JSONText
-    }).then((res)=>{
+    }).then((res) => {
         ToInnerHTML(res, '#LookedUpTitles');
     })
     //console.log(searchSubject);
@@ -25,86 +25,69 @@ function submitted() {
 function ToInnerHTML(data, element) {
     // insert Serialize? when needed?
     // console.log(data)
-
+console.log(data);
     // createElement
     // appendChild
-    for( let i = 0; i < 10; i++){
+    for (let i = 0; i < 10; i++) {
         //i < "number of displayed items"
         // console.log("nog iets" + i)
-        
+console.log(data.recipes[i].title);
         var newP = document.createElement("BUTTON");
-            newP.innerText = data.recipes[i].title;
-            var newP2 = document.createElement("H5");
-            newP2.innerText = data.recipes[i].ingredients;
-            
-         document.querySelector(element).appendChild(newP);
+        newP.innerText = data.recipes[i].title;
+        
+        
+       // var newP2 = document.createElement("H5");
+        //newP2.innerText = data.recipes[i].ingredients;
+        
+        newP.data = data.recipes[i];
+        newP.onclick= function(e){
+            console.log(this.data);
+        }
+
+        document.querySelector(element).appendChild(newP);
         // document.querySelector('.titlesIngredients').appendChild(newP2);
         //console.log(data.recipe[i].ingredients + " Ingredients found");
         // line above returns unidentified
-        };
+    };
 
 
 };
 
-
-
-
 function fireSearchForRecipe(searchSubject) {
     //return a new promise
-  
-   return new Promise(function (resolve, reject) {
-                //make new XMLHttpRequest();
-                var query = '&q=' + searchSubject;
-                var httpRequest = new XMLHttpRequest();
-                httpRequest.open('GET', url + query);
-                httpRequest.send();
-                //.log(httpRequest);
 
-                httpRequest.onload = function () {
-                    //This is called even on 404 etc
-                    // so check the status
+    return new Promise(function (resolve, reject) {
+        //make new XMLHttpRequest();
+        var query = '&q=' + searchSubject;
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.open('GET', url + query);
+        httpRequest.send();
+        //.log(httpRequest);
 
-                    if (httpRequest.status >= 200 && httpRequest.status < 400) {
-                        //Resolve the promise with the response text
-                        
-                        resolve(httpRequest.response)
-                        
-                        
-                    } else {
-                        //otherwise reject with the status text
-                        // which will be an error
-                        reject(httpRequest.status);
-                    };
-                };
-                //insert andere error
-                httpRequest.onerror = function(e){
-                    reject(e.status);
-                }
-            });
-            
-            // bla.then((res)=>{
-            //     console.log(res);
-            // });
-         
+        httpRequest.onload = function () {
+            //This is called even on 404 etc
+            // so check the status
 
-            //Handle network errors
-            
+            if (httpRequest.status >= 200 && httpRequest.status < 400) {
+                //Resolve the promise with the response text
 
-        
+                resolve(httpRequest.response)
 
-                
+
+            } else {
+                //otherwise reject with the status text
+                // which will be an error
+                reject(httpRequest.status);
             };
+        };
+        //insert andere error
+        httpRequest.onerror = function (e) {
+            reject(e.status);
+        }
+    });
 
-        // },
-        // function (error) {
-        //     console.error("Failed! this is an error", error);
-
-        // });
-
-
-
-
-
+    //Handle network errors
+};
 
 
 //Take a random number to choose a genre of food. 
@@ -112,36 +95,35 @@ function fireSearchForRecipe(searchSubject) {
 //outcommented because the result interferes with searchSubject
 // upon arriving on site.
 // need to add array with the 10 meals that will be displayed, currently 30+ are displayed.
-function recipeShowcase(){
+function recipeShowcase() {
 
     var genre = [
-        "meat", "fruit", "icecream", 
+        "meat", "fruit", "icecream",
         "protein", "chicken", "sushi",
         "beef", "pancake", "vegetable",
         "drink"
     ]
-    var x = Math.floor(Math.random()*genre.length);
+    var x = Math.floor(Math.random() * genre.length);
     var selectedGenre = genre[x];
-console.log("The value of x is " + selectedGenre);
+    console.log("The value of x is " + selectedGenre);
 
-//searchSubject = selectedGenre;
-fireSearchForRecipe(selectedGenre).then((res)=>{
-    // console.log(res);
-     var JSONText = JSON.parse(res)
-     return JSONText
- }).then((res)=>{
-     ToInnerHTML(res, '#ingredients');
- });
-
-
- };
+    //searchSubject = selectedGenre;
+    fireSearchForRecipe(selectedGenre).then((res) => {
+        // console.log(res);
+        var JSONText = JSON.parse(res)
+        return JSONText
+    }).then((res) => {
+        ToInnerHTML(res, '#ingredients');
+    });
 
 
- recipeShowcase();
+};
+
+
+recipeShowcase();
 
 
 // mapfilter
 //reduce
 //promises
 //
-
